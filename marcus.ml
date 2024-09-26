d4c3fb46620a06ff7f4e4b7b9a287c4efbad3d88
bollacm <bollacm@sunypoly.edu>
2024-09-26 18:02:12 -0400

    problems 5 6, 8, 9 completed and tested.

--- marcus.ml
+++ marcus.ml
@@ -1,3 +1,29 @@
+let list1 = [1;4;7;3];;
+let list2 = ['a';'b';'c';'d'];;
+let list3 = [1;0;4;0;5;6;1;0;3;0];;
+let list4 = dup list2;;
+
+
+
+
+
+(*Problem 5 Solution
+  Author: Marcus Bollacker*)
+
+let rec onezerocount list count =
+  match list with 
+  | [] -> count (*count needs to be passed*)
+  | 0 :: t -> onezerocount t (count + 1)
+  | 1 :: t -> onezerocount t (count + 1)
+  | _ :: t -> onezerocount t count;;
+
+
+onezerocount list3 0;;
+
+
+
+
+
 (*Problem 6 Solution
   Author: Marcus Bollacker*)
 
@@ -5,6 +31,9 @@ let rec makepairs x list =
   match list with 
   | [] -> []
   | h::t -> (x,h)::(makepairs x t);;
+
+makepairs 1 list1;;
+
 
 
 
@@ -15,7 +44,10 @@ let rec makepairs x list =
 let rec dup list =
   match list with
   | [] -> []
-  | h::t -> h :: h ::(dup t);;
+  | h::t -> h :: h :: (dup t);;
+
+dup list2;;
+
 
 
 
@@ -23,7 +55,12 @@ let rec dup list =
 (*Problem 9 Solution
   Author: Marcus Bollacker*)
 
-  let rec undup list =
-    match list with
-    | [] -> []
-    | [x] -> raise (Failure "bad input") 
+let rec undup list =
+  match list with
+  | [] -> []
+  | [h] -> raise (Failure "bad input") 
+  | h :: (h2 :: t) when h = h2 -> h :: undup t
+  | _ -> raise (Failure "bad input")
+
+undup list4;;
+undup list2;;
