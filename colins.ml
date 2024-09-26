1b61a60e30be5869d12cdd3037055864ac3be8be
swainc <swainc@sunypoly.edu>
2024-09-26 08:08:55 -0700

    prob 4 & 7

--- colins.ml
+++ colins.ml
@@ -3,7 +3,6 @@
 let x = [1; 3; 5; 7; 9];;
 let y = [];;
 let z =[3];;
-let w = [0; 1; 0; 2; 3; 0; 1];;
 
 let rec prob1 l =
     match l with
@@ -47,17 +46,28 @@ prob3 z;;
 
 (*Problem 4 Solution
   Author Colin Swain*)
-(*if hd =  y, concat tail remove head*)
 let rec prob4 l rmv = 
   match l with
   | [] -> []
-  | hd :: [] ->  
-    let 
+  | hd :: [] ->  if hd == rmv then [] else hd :: []
+  | hd :: tl -> if hd == rmv then prob4 tl rmv else hd :: prob4 tl rmv
 
-prob4 x;;
-prob4 y;;
-prob4 z;;
+prob4 x 3;;
+prob4 y 3;;
+prob4 z 3;;
 
-(*Problem 5 Solution
+(*Problem 7 Solution
   Author Colin Swain*)
-let prob5 l = 
+  (*One method uses the recursive, purely additive formula ( n k ) = ( n − 1 k − 1 ) + ( n − 1 k )
+  {\displaystyle {\binom {n}{k}}={\binom {n-1}{k-1}}+{\binom {n-1}{k}}} for all integers n , 
+  k {\displaystyle n,k} such that 1 ≤ k < n , {\displaystyle 1\leq k<n,} with boundary values ( n 0 )
+   = ( n n ) = 1 {\displaystyle {\binom {n}{0}}={\binom {n}{n}}=1} for all integers n ≥ 0. *)
+
+let rec prob7 n k =   
+  match n with 
+  | k -> 1
+  | n -> prob7 (n-1) (k-1) + prob7 (n-1) k
+
+prob7 1 2;;
+
+
